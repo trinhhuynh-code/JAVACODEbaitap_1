74624469aa44881b60cd6cecaf154be25c034841
Huynh Trinh <trinhhuynh@Huynhs-MacBook-Air.local>
2024-10-05 20:22:36 +0900

    Initial commit

---
+++ session2/FORLOOP/src/LAP4.java
@@ -0,0 +1,16 @@
+// Nhập vào số nguyện, kiểm tra xem có phải là số nguyên tố không, kết qủa trả về true or false
+
+import java.util.Scanner;
+public class LAP4 {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Nhập vào một số nguyên : ");
+        int num = scanner.nextInt();
+
+        boolean isCheck = False;
+
+
+        /////CHƯA HOÀN THÀNH/////
+    }
+}
---
+++ session2/FORLOOP/src/Print_1_25ofodd.java
@@ -0,0 +1,13 @@
+// in các số lẻ từ 1-25
+import java.util.Scanner;
+public class Print_1_25ofodd {
+    public static void main(String[] args) {
+
+        for (int i = 1; i<=25; i++){
+            if ( i % 2 == 1) {
+                System.out.println(i);
+            }
+        }
+    }
+}
+
